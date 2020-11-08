8fc8347dbbadbaf0f13c81dedb95f01c296d1c2b
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-11-08 15:27:06 -0500

    Added a view for leave a comment for a single post

--- controllers/landingpage-route.js
+++ controllers/landingpage-route.js
@@ -6,9 +6,15 @@ const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
 
   //checking whether the session is created or not
-  if (!req.session.loggedIn) {
-    req.session.loggedIn = false;
+  if (req.session.loggedIn) {
+   console.log("\n\n Result = "+req.session.loggedIn+"\n\n");
   }
+    else
+    {
+      req.session.loggedIn = false;
+      console.log("\n\n Bad Result= "+req.session.loggedIn+"\n\n");
+    }
+    
 
     
     Post.findAll({
@@ -101,6 +107,49 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.get('/single-post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('comment', {
+            post,
+            loggedIn: req.session.loggedIn, 
+            user_id: req.session.user_id
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 
   //crerated a route for dashbord
   router.get('/dashboard/:userId', (req, res) => {
